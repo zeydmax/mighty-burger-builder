5009d4d993821d6e1b7e320e2ca53fbbedc45c5b
maksim.repin <maksim.repin@motorsport.com>
2020-02-27 14:28:50 +0300

    Feature: asyncComponent Added asyncComponent hoc Checkout, Orders and Auth are now loaded asynchronously

--- src/hoc/asyncComponent/asyncComponent.js
+++ src/hoc/asyncComponent/asyncComponent.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react'
 
-const asyncComponent = wrappedComponent => {
+const asyncComponent = importComponent => {
   return class extends Component {
     state = {
       component: null,
     }
+
+    componentDidMount() {
+      importComponent().then(cmp => {
+        this.setState({component: cmp.default})
+      })
+    }
+    render() {
+      const C = this.state.component
+
+      return C ? <C {...this.props} /> : null
+    }
   }
 }
+
+export default asyncComponent
