503fd515d5ad735d36a1a057d7e2222766c75751
maksim.repin <maksim.repin@motorsport.com>
2020-08-27 14:49:38 +0300

    Feature: App Now passing props to routes with render prop

--- src/App.js
+++ src/App.js
@@ -34,9 +34,9 @@ const app = props => {
   if (props.isLoggedIn) {
     routes = (
       <Switch>
-        <Route path="/auth" render={() => <Auth />} />
-        <Route path="/checkout" render={() => <Checkout />} />
-        <Route path="/orders" render={() => <Orders />} />
+        <Route path="/auth" render={props => <Auth {...props} />} />
+        <Route path="/checkout" render={props => <Checkout {...props} />} />
+        <Route path="/orders" render={props => <Orders {...props} />} />
         <Route path="/logout" component={Logout} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
